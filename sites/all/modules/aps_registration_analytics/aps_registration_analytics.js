191d38073188f9208fda57f68b69b8e45f53f996
Moz's Mac <andrew.morris@thinkaps.com>
2015-01-19 17:50:22 +0000

    Removed unnecessary modules

--- sites/all/modules/aps_registration_analytics/aps_registration_analytics.js
+++ sites/all/modules/aps_registration_analytics/aps_registration_analytics.js
@@ -7,7 +7,7 @@
 
 Drupal.behaviors.apsAnalyticsReports = {
   attach: function (context, settings) {
-    $('#block-aps-registration-analytics-aps-analytics', context).show();
+    $('#block-aps-registration-analytics-aps-analytics, #block-aps-registration-analytics-common-browser, #block-aps-registration-analytics-geo-location', context).show();
     if ($('.aps-registration-analytics', context).length) {
       $.ajax({
         url: Drupal.settings.googleAnalyticsReportsAjaxUrl + '/aps-analytics',
@@ -15,6 +15,32 @@ Drupal.behaviors.apsAnalyticsReports = {
         data: ({ path: window.location.pathname + window.location.search }),
         success: function(data) {
           $('.aps-registration-analytics', context).html(data.content).hide().slideDown('fast');
+        },
+        error: function(data) {
+          // @TODO
+        }
+      });
+    }
+
+    if ($('.aps-common-browser', context).length) {
+      $.ajax({
+        url: Drupal.settings.googleAnalyticsReportsAjaxUrl + '/common-browser',
+        dataType: 'json',
+        success: function(data) {
+          $('.aps-common-browser', context).html(data.content);
+        },
+        error: function(data) {
+          // @TODO
+        }
+      });
+    }
+
+    if ($('.aps-geo-location', context).length) {
+      $.ajax({
+        url: Drupal.settings.googleAnalyticsReportsAjaxUrl + '/geo-location',
+        dataType: 'json',
+        success: function(data) {
+          $('.aps-geo-location', context).html(data.content);
         },
         error: function(data) {
           // @TODO
