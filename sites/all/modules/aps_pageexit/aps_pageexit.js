ebe0c212345ac9e09edd63902b885e67808a0778
Moz's Mac <andrew.morris@thinkaps.com>
2015-01-20 18:19:20 +0000

    Working Analytics Graphs

--- sites/all/modules/aps_pageexit/aps_pageexit.js
+++ sites/all/modules/aps_pageexit/aps_pageexit.js
@@ -1,26 +1,35 @@
 (function($) {
   Drupal.aps_pageexit = {};
-  var click = false; // Allow Submit/Edit button
+  var time = false;
 
   Drupal.behaviors.apsPageExit = {
     attach : function(context) {
       // Catch all links and buttons EXCEPT for "#" links
       //$("a, button, input[type='submit']:not(.node-edit-protection-processed)")
-      $("a, button, input[type='submit']")
-          .each(function() {
-            $(this).click(function() {
-              // return when a "#" link is clicked so as to skip the
-              // window.onbeforeunload function
-              if ($(this).attr("href") != "#") {
-                return 0;
-              }
-            });
-          });
+      $("a, button, input[type='submit']").each(function() {
+        $(this).click(function() {
+          // return when a "#" link is clicked so as to skip the
+          // window.onbeforeunload function
+          if ($(this).attr("href") != "#") {
+            return 0;
+          }
+        });
+      });
 
       // Handle backbutton, exit etc.
       window.onbeforeunload = function() {
-        if (!click) {
-          click = false;
+        if (!Drupal.settings.page_exit.begin && !Drupal.settings.page_exit.end) {
+          var begin = Date.parse(Drupal.settings.page_exit.start_time);
+          var end = Date.parse(Drupal.settings.page_exit.end_time);
+
+          var dt = new Date();
+          var now = Date.today().set({ hour: dt.getHours(), minute: dt.getMinutes(), seconds: dt.getSeconds()});
+
+          if (now > begin && end > now) {
+            time = true;
+          }
+        }
+        if (time) {
           return (Drupal.t("This webcast has not ended yet."));
         }
       }
