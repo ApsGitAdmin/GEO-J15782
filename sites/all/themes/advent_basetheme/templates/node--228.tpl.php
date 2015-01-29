d8440855aa3ce72986f87e43a1b87f7ef29a4dd8
Moz's Mac <andrew.morris@thinkaps.com>
2015-01-29 11:51:06 +0000

    Live Polling template page

---
+++ sites/all/themes/advent_basetheme/templates/node--228.tpl.php
@@ -0,0 +1,35 @@
+<article<?php print $attributes; ?>>
+  <?php print $user_picture; ?>
+  <?php print render($title_prefix); ?>
+  <h2><?php print $title; ?></h2>
+  <?php if (!$page && $title): ?>
+  <header>
+    <h2<?php print $title_attributes; ?>><a href="<?php print $node_url ?>" title="<?php print $title ?>"><?php print $title ?></a></h2>
+  </header>
+  <?php endif; ?>
+  <?php print render($title_suffix); ?>
+  
+  
+  <div<?php print $content_attributes; ?>>
+<script src="http://www.polleverywhere.com/multiple_choice_polls/OhvUpLxtl572eSi/web.js?height=250&results_count_format=percent&width=300"></script>
+    <?php
+      // We hide the comments and links now so that we can render them later.
+      hide($content['comments']);
+      hide($content['links']);
+      print render($content);
+    ?>
+  </div>
+  <?php if ($display_submitted): ?>
+  <footer class="submitted"><?php echo "Posted: " . date( "F j, Y",$node->created);?></footer>
+  <?php endif; ?>  
+  <div class="clearfix">
+    <?php if (!empty($content['links'])): ?>
+      <nav class="links node-links clearfix"><?php print render($content['links']); ?></nav>
+    <?php endif; ?>
+
+    
+    <?php print render($content['comments']['comment_form']); ?>
+
+
+  </div>
+</article>
